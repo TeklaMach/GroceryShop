aa7094f05502dc65989fbd3025f88c8f270bf0c5
TeklaMach <macharashvilitekla@gmail.com>
2023-12-10 14:20:08 +0400

    Initial Commit

---
+++ GroceryShop/GroceryShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryShopApp.swift
+//  GroceryShop
+//
+//  Created by Tekla Matcharashvili on 10.12.23.
+//
+
+import SwiftUI
+
+@main
+struct GroceryShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
